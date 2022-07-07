84162d9173e2ca81f5cba4088b37b05eaa090152
hezex <hezexi520@163.com>
2022-07-07 18:20:52 +0800

    2nd commit

--- condef.h
+++ condef.h
@@ -1,4 +1,5 @@
 /**
  * This file defined most macros which is useful in condows
- * 
+ * Copyright by hezex. Reach me at hezexi520@163.com
+ * No warranty is given. Condows is open source on Github
 */
--- condows.h
+++ condows.h
@@ -16,7 +16,7 @@ Be careful of 'Redefine'
  * This file integrated the condows pack.
  * Copyright by hezex. Reach me at hezexi520@163.com
  * No warranty is given. Condows is open source on Github
- * You can get the latest version on 
+ * You can get the latest version on https://github.com/hezex/condows
  */
 #include <iostream>
 #include <cstdio>
